fd975834677073c49dbfbd3ff3232119529039ba
GiselleGewelle <satavamas@mail.ru>
2023-06-26 21:04:34 +0600

    added

--- category/views.py
+++ category/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics, permissions
+from . import serializers
+from .models import Category
+
+
+class CategoryCreateListView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = serializers.CategorySerializer
+
+    # permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+    #                       permissions.IsAdminUser)
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return permissions.AllowAny(),
+        return permissions.IsAdminUser(),
+
+
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = serializers.CategorySerializer
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [permissions.AllowAny(), ]
+        return [permissions.IsAdminUser(), ]
