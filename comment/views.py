7b2f3fb46587bea826fefbc79ecc9a9fae3fb087
bakberdi <charmed-9@mail.ru>
2023-06-27 18:42:21 +0600

    add

--- comment/views.py
+++ comment/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics, permissions
+
+from posts import permissions
+from post.permissions import ISAuthorOrAdminOrPostOwner
+from .models import Comment
+from . import serializers
+
+
+class CommentCreateView(generics.CreateAPIView):
+    serializer_class = serializers.CommentSerializer
+    permission_classes = (permissions.IsAuthenticated, )
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [ISAuthorOrAdminOrPostOwner(), ]
+        return [permissions.AllowAny(), ]
+
--- like/models.py
+++ like/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 
-# from post.models import Post
+from post.models import Post
 
 
-# class Like(models.Model):
-#     owner = models.ForeignKey('auth.User', related_name='likes', on_delete=models.CASCADE)
-#     post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
-#
-#     class Meta:
-#         unique_together = ['owner', 'post']
-#
-#
-# class Favorite(models.Model):
-#     owner = models.ForeignKey('auth.User', related_name='favorites', on_delete=models.CASCADE)
-#     post = models.ForeignKey(Post, related_name='favorites', on_delete=models.CASCADE)
-#
-#     class Meta:
-#         unique_together = ['owner', 'post']
+class Like(models.Model):
+    owner = models.ForeignKey('auth.User', related_name='likes', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ['owner', 'post']
+
+
+class Favorite(models.Model):
+    owner = models.ForeignKey('auth.User', related_name='favorites', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='favorites', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ['owner', 'post']
